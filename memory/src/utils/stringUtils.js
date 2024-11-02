1e6c825fad21632d5813efb644213c56348f316b
flex-57 <129852644+flex-57@users.noreply.github.com>
2024-11-02 16:29:03 +0100

    Fonction Majuscules pour les nom prénom

--- memory/src/utils/stringUtils.js
+++ memory/src/utils/stringUtils.js
@@ -1,3 +1,7 @@
 export const ucFirst = str => {
     return str[0].toUpperCase() + str.slice(1).toLowerCase()
 }
+
+export const capitalizeName = str => {
+    return str.split(' ').map(ucFirst).join(' ')
+}
